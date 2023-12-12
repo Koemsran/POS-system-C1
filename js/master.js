8deebdaf800ac20f5c3671239931f12bbd8ad5a0
KOEMSRAN.PHON <KOEMSRAN.PHON@PNC.LAN>
2023-12-12 15:16:16 +0700

    #4 create home page

--- js/master.js
+++ js/master.js
@@ -1,4 +1,5 @@
-//Main 
+//<============== MAIN OF FORM LOGIN ==================>
+
 let userName = document.querySelector('#user-name');
 let password = document.querySelector('#user-password');
 let btnLogin = document.querySelector('.btn-login button');
@@ -6,12 +7,16 @@ let container = document.querySelector(".container");
 let passError = document.querySelector('.pass-error');
 let nameError = document.querySelector('.name-error');
 let startLogin = document.querySelector('.start-login');
-let iconEye = document.querySelector(".psw a ");
+let iconEye = document.querySelector(".input-pass .off");
+let showPassIcon = document.querySelector('.input-pass .on');
+
+
+//<================ LOG IN FUNCTION ===================>
 
 function logIn(e) {
     e.preventDefault()
-    if (userName.value === "pos" && password.value == 1234) {
-        show(container);
+    if (userName.value === "pos" && password.value === "1234") {
+        window.location.href = 'http://127.0.0.1:5500/pages/home/home.html'
         hide(startLogin)
         hide(nameError)
         hide(passError)
@@ -21,12 +26,11 @@ function logIn(e) {
     } else {
         show(nameError)
     }
-    if (password.value === 1234) {
+    if (password.value === "1234") {
         hide(passError);
 
     } else {
         show(passError)
-        iconEye.style.top = ': -100px'
 
     }
     userName.value = ''
@@ -34,7 +38,38 @@ function logIn(e) {
 
 }
 
+//<================ SHOW PASSWORD FUNCTION ===================>
+
+function showPassword(e){
+    e.preventDefault()
+    if(password.type === 'password'){
+        password.type = 'text';
+        hide(iconEye)
+        show(showPassIcon)
+
+    }
+}
+
+//<================ HIDE PASSWORD FUNCTION ===================>
+
+function hihdePassword(e){
+    e.preventDefault()
+    if(password.type !== 'password'){
+        password.type = 'password';
+        show(iconEye)
+        hide(showPassIcon)
+
+    }
+
+}
+
+//===================== Eventlistener ========================
+
 btnLogin.addEventListener('click', logIn)
+iconEye.addEventListener('click', showPassword)
+showPassIcon.addEventListener('click', hihdePassword)
+
+//<================ HIDE AND SHOW SOMETHING FUNCTION ===================>
 
 function show(element) {
     element.style.display = 'block';
@@ -43,3 +78,6 @@ function show(element) {
 function hide(element) {
     element.style.display = 'none';
 }
+
+
+
---
+++ pages/home/home.js
@@ -0,0 +1,13 @@
+//<============== MAIN OF FORM LOGIN ==================>
+
+//<================ HIDE AND SHOW SOMETHING FUNCTION ===================>
+
+function show(element) {
+    element.style.display = 'block';
+}
+
+function hide(element) {
+    element.style.display = 'none';
+}
+
+
