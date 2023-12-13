673fbfcd1988110f639028651a74c62db6f694a1
thary.oeun <thary.oeun@student.passerellernumeriques.org>
2023-12-13 20:46:06 +0700

    #6 category

---
+++ pages/category/category.js
@@ -0,0 +1,11 @@
+let addNewCategory = document.querySelector('')
+
+
+function onAddNewCategory(){
+
+}
+
+let btn = document.querySelector('button');
+btn.addEventListener('click', addNewCategory);
+
+console.log(btn);
