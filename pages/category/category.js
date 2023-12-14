435e6a72652c32c15facc17a253e39e52f897a91
thary.oeun <thary.oeun@student.passerellernumeriques.org>
2023-12-14 20:48:04 +0700

    #6 create category

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,11 +1,71 @@
-let addNewCategory = document.querySelector('')
 
 
-function onAddNewCategory(){
+// Function to render categories
+function renderCategory() {
+    let createTbody = document.createElement('tbody');
 
+    
+    for (let listCategory of category) {
+        let tdRow = document.createElement('tr');
+
+        let tdId = document.createElement('td');
+        tdId.textContent = "000" + listCategory.id;
+
+        let tdName = document.createElement('td');
+        tdName.textContent = listCategory.name;
+
+        let tdQuan = document.createElement('td');
+        tdQuan.textContent = listCategory.quan;
+
+        let tdAction = document.createElement('td');
+        tdAction.textContent = listCategory.action;
+
+        tdRow.appendChild(tdId);
+        tdRow.appendChild(tdName);
+        tdRow.appendChild(tdQuan);
+        tdRow.appendChild(tdAction);
+
+        createTbody.appendChild(tdRow);
+    }
+
+    table.appendChild(createTbody);
 }
 
-let btn = document.querySelector('button');
-btn.addEventListener('click', addNewCategory);
 
-console.log(btn);
+function addNewCategory() {
+  
+}
+
+
+function onCancelButton(event) {
+    event.preventDefault(); 
+    addCategory.style.display = 'none'; 
+}
+
+function onAddButton(event) {
+    event.preventDefault(); 
+    addCategory.style.display = 'block'; 
+}
+
+
+function clearForm() {
+    
+}
+
+
+let table = document.getElementById("table");
+let buttonAdd = document.getElementById("btn-add");
+let CancelBtn = document.getElementById("btn-cancel");
+
+
+buttonAdd.addEventListener('click', addNewCategory);
+CancelBtn.addEventListener('click', onCancelButton);
+addCategory.addEventListener('click', onAddButton);
+
+
+let inputCategoryId = document.getElementById('id');
+let inputCategoryName = document.getElementById('name');
+let inputCategoryQuan = document.getElementById('quan');
+let inputCategoryAction = document.getElementById('action');
+
+renderCategory(); 
