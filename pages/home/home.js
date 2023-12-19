5d0ef40ed1761baca8d2a143439cbf401a911ca1
KOEMSRAN.PHON <KOEMSRAN.PHON@PNC.LAN>
2023-12-19 20:40:47 +0700

    #0

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,20 +1,31 @@
 import { getElement, getElements, saveData, laodData, createElement, show, hide } from "../../js/locialstorage.js";
 
+let incomeValue = laodData('historyData') || []
 // ==============> GET CALUE FROM HTML <===============
 let instock = getElement('#c1');
 let category = getElement('#c2');
 let soldout = getElement('#c3');
 let income = getElement('#c4');
+
 // ==============> GET DATA FROM LOCALSTOREAGE <===============
 let dataStore = laodData('dataStore');
-let dataSold = laodData('dataSoldout');
-let dataIcome = laodData('dataIcome');
-let Income = 0;
+
+
 // ==============> GET VALUE TO CART <===============
-category.textContent = dataStore.categories.length;
-instock.textContent = dataStore.instock;
-soldout.textContent = dataSold.totalqty;
-for(let value of dataIcome.income){
-    Income+=parseInt(value)
+let sumCategory = 0;
+for (let value of dataStore.categories){
+    sumCategory++
 }
-income.textContent = Income + '$';
+category.textContent = sumCategory;
+
+let result = 0;
+for (let value of incomeValue) {
+    result += value;
+}
+income.textContent = result;
+
+let store = 0;
+for (let obj of dataStore.products) {
+    store += parseInt(obj.quantity)
+}
+instock.textContent = store;
--- pages/order/order.js
+++ pages/order/order.js
@@ -1,216 +1,194 @@
-import { getElement, getElements, saveData, laodData, removeData, createElement, show, hide } from "../../js/locialstorage.js";
-
-// ===============> LOCAL VARIBLE <=============
-let dataStore = laodData('dataStore');
-let listId = [];
-let dataCheckout = {
+import {
+    getElement,
+    getElements,
+    saveData,
+    laodData,
+    createElement,
+    show,
+    hide,
+    removeData,
+  } from "../../js/locialstorage.js";
+  
+  // ===============> LOCAL VARIBLE <=============
+  let dataStore = laodData("dataStore");
+  let historyData = [];
+  let listId = [];
+  let dataCheckout = {
     cart: [],
-    total: 0,
-}
-let dataSoldout = {
-    sold: [],
-    totalqty: 0,
-}
-let dataIcome = {
-    income: []
-}
-if (laodData('dataCheckout') !== null) {
-    dataCheckout = laodData('dataCheckout');
-}
-//==============> FUNCTION <==================
-function checkId() {
-
+  };
+  if (laodData("dataCheckout") !== null) {
+    dataCheckout = laodData("dataCheckout");
+  }
+  if (laodData("historyData") !== null) {
+      historyData = laodData("historyData");
+  }
+  //==============> FUNCTION <==================
+  function checkId() {
     for (let ID of dataStore.products) {
-        listId.push(ID.id)
-
+      listId.push(ID.id);
     }
     if (listId.includes(parseInt(searchId.value))) {
-        hide(alert)
-        let productIndex = dataStore.products.findIndex(product => product.id === parseInt(searchId.value));
-        let obj = dataStore.products[productIndex];
-        namePro.textContent = obj.name;
-        qty.textContent = obj.quantity;
-        price.textContent = obj.grossprice + "$";
-    }
-    else {
-        show(alert)
-        clearrForm()
-
-    }
-    if (searchId.value === '') {
-        hide(alert)
-
-    }
-    if (namePro.textContent === "" || qty.textContent === "" || price.textContent === "") {
-        btnAdd.disabled = true;
-        btnAdd.style.background = 'gray';
-
+      hide(alert);
+      let productIndex = dataStore.products.findIndex(
+        (product) => product.id === parseInt(searchId.value)
+      );
+      let obj = dataStore.products[productIndex];
+      namePro.textContent = obj.name;
+      qty.textContent = obj.quantity;
+      price.textContent = obj.grossprice + "$";
     } else {
-        btnAdd.disabled = false;
-        btnAdd.style.background = 'green';
-
+      show(alert);
+      clearrForm();
     }
-
-}
-
-function addCart() {
+    if (searchId.value === "") {
+      hide(alert);
+    }
+  }
+  
+  function addCart() {
+    if (searchId.value === "") return window.alert("Please enter product id");
     let list = {
-        id: parseInt(searchId.value),
-        name: namePro.textContent,
-        quantity: parseInt(qty.textContent),
-        price: parseInt(price.textContent),
-        addquan: 1
-    }
-    dataCheckout.cart.push(list)
-    saveData('dataCheckout', dataCheckout)
-    if (dataCheckout.cart[0] === null) {
-        show(printReciept)
-    }
+      id: parseInt(searchId.value),
+      name: namePro.textContent,
+      quantity: parseInt(qty.textContent),
+      price: parseInt(price.textContent),
+    };
+    dataCheckout.cart.push(list);
+    saveData("dataCheckout", dataCheckout);
     window.location.reload();
-}
-
-function renderCart() {
-    searchId.value = ''
-    hide(message)
-    clearrForm()
-    tbody.remove()
-    let newTbody = createElement('tbody');
-    newTbody.className = 'tbody';
+  }
+  
+  function renderCart() {
+    searchId.value = "";
+  
+    hide(message);
+    clearrForm();
+    tbody.remove();
+    let newTbody = createElement("tbody");
+    newTbody.className = "tbody";
     let totalPrice = 0;
-    let totalquan = 0;
     let index = 0;
     for (let data of dataCheckout.cart) {
-        let tRow = createElement('tr');
-
-        let tdId = createElement('td');
-        let tdName = createElement('td');
-        let tdPrice = createElement('td');
-        let tdAtion = createElement('td');
-        let tdQuan = createElement('td');
-        tdQuan.dataset.index = index
-        tdQuan.className = 'tdQuan';
-        tdId.dataset.id = index;
-
-        let qty = createElement('input')
-        qty.className = 'Qty'
-        qty.type = 'number';
-        qty.value = data.addquan;
-        tdQuan.appendChild(qty)
-        qty.addEventListener('change', updateQuantity)
-
-
-        tdId.textContent = "#00"+data.id;
-        tdName.textContent = data.name;
-        tdPrice.textContent = data.price + "$";
-
-        let btnRemove = createElement('div');
-        let iconRemove = createElement("span");
-        iconRemove.className = 'remove material-symbols-outlined';
-        iconRemove.textContent = "delete"
-        btnRemove.appendChild(iconRemove)
-        tdAtion.appendChild(btnRemove)
-
-        tRow.appendChild(tdId);
-        tRow.appendChild(tdName);
-        tRow.appendChild(tdQuan);
-        tRow.appendChild(tdPrice);
-        tRow.appendChild(tdAtion);
-        newTbody.appendChild(tRow);
-        table.appendChild(newTbody)
-        totalPrice += (parseInt(tdPrice.textContent) * parseInt(qty.value));
-        totalquan += parseInt(qty.value)
-        getBtn(newTbody)
-        index++;
+      let tRow = createElement("tr");
+  
+      let tdId = createElement("td");
+      let tdName = createElement("td");
+      let tdPrice = createElement("td");
+      let tdAtion = createElement("td");
+      let tdQuan = createElement("td");
+  
+      tdQuan.className = "tdQuan";
+      tdId.dataset.id = index;
+  
+      let qty = createElement("input");
+      qty.className = "Qty";
+      qty.type = "number";
+      qty.value = 1;
+      tdQuan.appendChild(qty);
+      qty.addEventListener("change", updateQuantity);
+  
+      tdId.textContent = data.id;
+      tdName.textContent = data.name;
+      tdPrice.textContent = data.price + "$";
+  
+      let btnRemove = createElement("div");
+      let iconRemove = createElement("span");
+      iconRemove.className = "remove material-symbols-outlined";
+      iconRemove.textContent = "delete";
+      btnRemove.appendChild(iconRemove);
+      tdAtion.appendChild(btnRemove);
+  
+      tRow.appendChild(tdId);
+      tRow.appendChild(tdName);
+      tRow.appendChild(tdQuan);
+      tRow.appendChild(tdPrice);
+      tRow.appendChild(tdAtion);
+      newTbody.appendChild(tRow);
+      table.appendChild(newTbody);
+      totalPrice += parseInt(tdPrice.textContent);
+      getBtn(newTbody);
+      index++;
     }
-    total.textContent = parseInt(totalPrice) + '$';
-    dataSoldout.totalqty = totalquan;
-    dataCheckout.total = parseInt(totalPrice) + '$';
-
-
-}
-
-function clearrForm() {
+    total.textContent = parseInt(totalPrice) + "$";
+  }
+  
+  function clearrForm() {
     namePro.textContent = "";
     qty.textContent = "";
     price.textContent = "";
-}
-
-function updateQuantity(e) {
+  }
+  
+  function updateQuantity(e) {
     let qty = e.target.value;
-    let cartId = e.target.closest('td').dataset.index;
-    if (qty > dataCheckout.cart[cartId].quantity) {
-        window.alert('Product not enough')
-        qty = qty - 1;
+    let cartId = e.target.closest("td").dataset.id;
+    let Index = dataCheckout.cart.findIndex(
+      (list) => parseInt(list.id) === parseInt(cartId)
+    );
+    if (qty > dataCheckout.cart[Index].quantity) {
+      window.alert("Product not enough");
     }
-    if (qty <= 0) {
-        window.alert('Cannot accept');
-        qty = parseInt(qty) + 1;
-
-    }
-    dataCheckout.cart[cartId].addquan = qty;
-    saveData('dataCheckout', dataCheckout)
-    renderCart()
-    window.location.reload()
-
-}
-
-function removeElement(event) {
-    let indexTr = event.target.closest('tr');
+  
+    saveData("dataCheckout", dataCheckout);
+    renderCart();
+  }
+  
+  function removeElement(event) {
+    let indexTr = event.target.closest("tr");
     let productId = indexTr.firstElementChild.dataset.id;
-    let isRemove = window.confirm('Do you want to delete all products?');
+    let isRemove = window.confirm("Do you want to delete all products?");
     if (isRemove) {
-        indexTr.remove()
-        dataCheckout.cart.splice(productId, 1);
-        saveData('dataCheckout', dataCheckout)
+      indexTr.remove();
+      dataCheckout.cart.splice(productId, 1);
+      saveData("dataCheckout", dataCheckout);
     }
-    window.location.reload()
-
-
-}
-
-function getBtn(tbody) {
-    let btnRemove = tbody.lastElementChild.lastElementChild.lastElementChild.lastElementChild;
-    btnRemove.addEventListener('click', removeElement);
-}
-
-function printer() {
-    show(printReciept)
-}
-
-function soldOut() {
-    dataSoldout.sold = dataCheckout.cart;
-    dataIcome.income.push(dataCheckout.total);
-    saveData('dataSoldout', dataSoldout)
-    saveData('dataIcome', dataIcome)
-    removeData('dataCheckout')
-    window.location.reload()
-
-
+  }
+  
+  function getBtn(tbody) {
+    let btnRemove =
+      tbody.lastElementChild.lastElementChild.lastElementChild.lastElementChild;
+    btnRemove.addEventListener("click", removeElement);
+  }
+  function printer() {
+    show(printReciept);
+  }
+  function checkoutProduct() {
+    let income = total.textContent.replace("$", "");
+    historyData.push(parseInt(income));
+    saveData("historyData",historyData);
+    // for (let checkout of dataCheckout.cart) {
+    //     let productId = dataStore.products.findIndex(pro => console.log(pro.id));
+    //     console.log(productId);
+    // }
+    removeData("dataCheckout");
+    window.location.reload();
 }
 // ===============> GET ELEMENT <================
-let searchId = getElement('#search');
+let searchId = getElement("#search");
 let namePro = getElement(".search-pro");
-let qty = getElement('#qty span');
-let price = getElement("#price-pro")
+let qty = getElement("#qty span");
+let price = getElement("#price-pro");
 let alert = getElement("#search-bar span");
-let btnAdd = getElement('.checkout .btn');
-let tbody = getElement('.tbody');
-let table = getElement('#table')
-let total = getElement('.total')
-let message = getElement('.message');
-let btnPrint = getElement('.print');
-let printReciept = getElement('#print-reciept');
-const btn_print = getElement('.btn-print');
-let btnCheck = getElement('.check')
+let btnAdd = getElement(".checkout .btn");
+let tbody = getElement(".tbody");
+let table = getElement("#table");
+let total = getElement(".total");
+let message = getElement(".message");
+let btnPrint = getElement(".print");
+let printReciept = getElement("#print-reciept");
+const btn_print = getElement(".btn-print");
+
+let checkoutBtn = getElement(".check");
 
 //==============>ADD EVENLISTENER <================
 
-btn_print.addEventListener('click', () => {
-    window.print()
+btn_print.addEventListener("click", () => {
+  window.print();
 });
 
-searchId.addEventListener('keyup', checkId);
-btnAdd.addEventListener('click', addCart);
-btnPrint.addEventListener('click', printer);
-btnCheck.addEventListener('click', soldOut)
-renderCart()
+searchId.addEventListener("keyup", checkId);
+btnAdd.addEventListener("click", addCart);
+btnPrint.addEventListener("click", printer);
+// laodData("dataCheckout")
+renderCart();
+
+checkoutBtn.addEventListener('click', checkoutProduct)
